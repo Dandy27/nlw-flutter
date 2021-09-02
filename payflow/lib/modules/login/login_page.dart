6064854279e56779929d5cd1d5912cf64378ec5b
dandy <danielsbarbosa27@gmail.com>
2021-09-02 14:57:17 -0300

    log no firebase ok

--- payflow/lib/modules/login/login_page.dart
+++ payflow/lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:payflow/shared/themes/app_collors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_style.dart';
@@ -58,8 +59,8 @@ class _LoginPageState extends State<LoginPage> {
                     padding:
                         const EdgeInsets.only(left: 40, right: 40, top: 30),
                     child: SocialLoginbutton(
-                      onTap: () {
-                        print('clicou');
+                      onTap: () async {
+                       
                       },
                     ),
                   )
