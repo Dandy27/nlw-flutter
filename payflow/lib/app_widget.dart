bc5060869b6d2b3426dab35a094eff208cd61c37
dandy <danielsbarbosa27@gmail.com>
2021-09-05 02:11:05 -0300

    ajuste

--- payflow/lib/app_widget.dart
+++ payflow/lib/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'modules/barcode_scanner/barcode_scanner_page.dart';
 import 'modules/home/home_page.dart';
@@ -7,14 +8,19 @@ import 'modules/splash.dart';
 import 'shared/themes/app_collors.dart';
 
 class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pay Flow',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.orange,
-        primaryColor: AppColors.primary),
+          primarySwatch: Colors.orange, primaryColor: AppColors.primary),
       initialRoute: '/splash',
       routes: {
         "/splash": (context) => Splash(),
